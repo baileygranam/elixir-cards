e27c4d735d8ff5b4bdb4b9d1addd397f070915cb
Bailey Granam <bgranam@stetson.edu>
2019-05-17 13:16:15 -0400

    added first unit test

--- lib/cards.ex
+++ lib/cards.ex
@@ -8,23 +8,12 @@ defmodule Cards do
 
   ## Examples
 
-      iex> Cards.create_deck()
-      ["Ace of Diamonds", "Ace of Hearts", "Ace of Spades", "Ace of Clubs",
-      "Two of Diamonds", "Two of Hearts", "Two of Spades", "Two of Clubs",
-      "Three of Diamonds", "Three of Hearts", "Three of Spades", "Three of Clubs",
-      "Four of Diamonds", "Four of Hearts", "Four of Spades", "Four of Clubs",
-      "Five of Diamonds", "Five of Hearts", "Five of Spades", "Five of Clubs",
-      "Six of Diamonds", "Six of Hearts", "Six of Spades", "Six of Clubs",
-      "Seven of Diamonds", "Seven of Hearts", "Seven of Spades", "Seven of Clubs",
-      "Eight of Diamonds", "Eight of Hearts", "Eight of Spades", "Eight of Clubs",
-      "Nine of Diamonds", "Nine of Hearts", "Nine of Spades", "Nine of Clubs",
-      "Jack of Diamonds", "Jack of Hearts", "Jack of Spades", "Jack of Clubs",
-      "King of Diamonds", "King of Hearts", "King of Spades", "King of Clubs",
-      "Queen of Diamonds", "Queen of Hearts", "Queen of Spades", "Queen of Clubs"]
+      iex> length(Cards.create_deck)
+      52
 
   """
   def create_deck do
-    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "King", "Queen"]
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "King", "Queen"]
     suites = ["Diamonds", "Hearts", "Spades", "Clubs"]
 
     # Get every combination of suites and values
