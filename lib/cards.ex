ce5a7b6c9ce3b483f9a8dd3d867229500a3f3cf3
Bailey Granam <bgranam@stetson.edu>
2019-05-17 10:36:27 -0400

    created function to generate a deck of cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,32 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
 
   @doc """
-  Hello world.
+  Generates a deck of cards.
 
   ## Examples
 
-      iex> Cards.hello()
-      :world
+      iex> Cards.create_deck()
+      ["Ace of Diamonds", "Ace of Hearts", "Ace of Spades", "Ace of Clubs",
+      "Two of Diamonds", "Two of Hearts", "Two of Spades", "Two of Clubs",
+      "Three of Diamonds", "Three of Hearts", "Three of Spades", "Three of Clubs",
+      "Four of Diamonds", "Four of Hearts", "Four of Spades", "Four of Clubs",
+      "Five of Diamonds", "Five of Hearts", "Five of Spades", "Five of Clubs",
+      "Six of Diamonds", "Six of Hearts", "Six of Spades", "Six of Clubs",
+      "Seven of Diamonds", "Seven of Hearts", "Seven of Spades", "Seven of Clubs",
+      "Eight of Diamonds", "Eight of Hearts", "Eight of Spades", "Eight of Clubs",
+      "Nine of Diamonds", "Nine of Hearts", "Nine of Spades", "Nine of Clubs",
+      "Jack of Diamonds", "Jack of Hearts", "Jack of Spades", "Jack of Clubs",
+      "King of Diamonds", "King of Hearts", "King of Spades", "King of Clubs",
+      "Queen of Diamonds", "Queen of Hearts", "Queen of Spades", "Queen of Clubs"]
 
   """
-  def hello do
-    :world
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "King", "Queen"]
+    suites = ["Diamonds", "Hearts", "Spades", "Clubs"]
+
+    # Get every combination of suites and values
+    for value <- values, suit <- suites do
+        "#{value} of #{suit}"
+    end
   end
 end
